de3309d61387ef4315bceb6ef50f268d62001761
Richard Gilbert <gilbert67@gmail.com>
2020-02-21 16:47:20 -0800

    Rest of the initial stuff.

---
+++ films-past.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Past Films
+*/
+
+get_header();
+$container   = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+  <div class="wrapper" id="single-wrapper">
+
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+      <div class="row">
+
+        <main class="site-main" id="main">
+
+          <?php
+
+          while ( have_posts() ) : the_post();
+            the_title( '<h1 class="entry-title">', '</h1>' );
+            endwhile; // end of the loop.
+
+          ?>
+
+        </main><!-- #main -->
+
+      </div><!-- .row -->
+
+    </div><!-- Container end -->
+
+  </div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
